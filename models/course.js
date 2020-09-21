c89ae1a279ffb28cc468f15dafcaac37ce0c8a60
CyberPunk10 <konstantin21_arx@gmail.com>
2020-09-21 19:57:19 +0300

    Add Card

--- models/course.js
+++ models/course.js
@@ -21,7 +21,6 @@ class Course {
       img: this.img,
       id: this.id
     })
-    console.log(this.id, this.title)
 
     return new Promise((resolve, reject) => {
       fs.writeFile(
@@ -55,9 +54,32 @@ class Course {
   }
 
   static async getById(id) {
-    const courses = Course.getAll()
+    const courses = await Course.getAll()
     return courses.find(c => c.id === id)
   }
+  
+  static async update(course) {
+    const courses = await Course.getAll()
+
+    const idx = courses.findIndex(c => c.id === course.id)
+    courses[idx] = course
+    
+    return new Promise((resolve, reject) => {
+      fs.writeFile(
+        path.join(__dirname, '..', 'data', 'courses.json'),
+        JSON.stringify(courses),
+        (err) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve()
+          }
+        }
+      )
+    })
+  }
+
 }
+
 
 module.exports = Course
--- routes/courses.js
+++ routes/courses.js
@@ -1,5 +1,5 @@
 const { Router } = require('express')
-const Course = require('../models/course')
+const Course = require('../models/Course')
 const router = Router()
 
 router.get('/', async (req, res) => {
@@ -14,9 +14,28 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   const course = await Course.getById(req.params.id)
   res.render('course', {
+    layout: 'empty',
     title: `Курс ${course.title}`,
     course
   })
+})
+
+router.get('/:id/edit', async (req, res) => {
+  if (!req.query.allow) {
+    return res.redirect('/')
+  }
+
+  const course = await Course.getById(req.params.id)
+
+  res.render('course-edit', {
+    title: `Редактировать ${course.title}`,
+    course
+  })
+})
+
+router.post('/edit', async (req, res) => {
+  await Course.update(req.body)
+  res.redirect('/courses')
 })
 
 module.exports = router
