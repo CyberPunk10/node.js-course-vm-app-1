fa82d333a0b117f548959ea8ed959973d8ea866e
CyberPunk10 <konstantin21_arx@gmail.com>
2020-09-23 20:00:10 +0300

    Falid to compile.. Надо поправить ошибки

--- models/course.js
+++ models/course.js
@@ -1,85 +1,12 @@
-const path = require('path')
-const fs = require('fs')
+const { Schema, model } = require('mongoose')
 
-const { v4: uuidv4 } = require('uuid');
-uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+const course = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  price: Number,
+  img: String
+})
 
-
-class Course {
-  constructor(title, price, img) {
-    this.title = title
-    this.price = price
-    this.img = img
-    this.id = uuidv4()
-  }
-
-  async save() {
-    const courses = await Course.getAll()
-    courses.push({
-      title: this.title,
-      price: this.price,
-      img: this.img,
-      id: this.id
-    })
-
-    return new Promise((resolve, reject) => {
-      fs.writeFile(
-        path.join(__dirname, '..', 'data', 'courses.json'),
-        JSON.stringify(courses),
-        (err) => {
-          if (err) {
-            reject(err)
-          } else {
-            resolve()
-          }
-        }
-      )
-    })
-  }
-
-  static getAll() {
-    return new Promise((resolve, reject) => {
-      fs.readFile(
-        path.join(__dirname, '..', 'data', 'courses.json'),
-        'utf-8',
-        (err, content) => {
-          if (err) {
-            reject(err)
-          } else {
-            resolve(JSON.parse(content))
-          }  
-        }
-      )
-    }) 
-  }
-
-  static async getById(id) {
-    const courses = await Course.getAll()
-    return courses.find(c => c.id === id)
-  }
-  
-  static async update(course) {
-    const courses = await Course.getAll()
-
-    const idx = courses.findIndex(c => c.id === course.id)
-    courses[idx] = course
-    
-    return new Promise((resolve, reject) => {
-      fs.writeFile(
-        path.join(__dirname, '..', 'data', 'courses.json'),
-        JSON.stringify(courses),
-        (err) => {
-          if (err) {
-            reject(err)
-          } else {
-            resolve()
-          }
-        }
-      )
-    })
-  }
-
-}
-
-
-module.exports = Course
+module.exports = model('Course', course)
